d26f80b7e7acf91944af61f2d3c31fbcb73ef3d0
Yohannes-Asfaw <jonathanasfaw11@gmail.com>
2024-03-19 11:32:24 +0300

    fix

--- unit_test.py
+++ unit_test.py
@@ -7,7 +7,6 @@ class TestProduct(unittest.TestCase):
         # Create a Product instance
         product1 = product.Product("Laptop", 999.99, 2)
 
-        # Call the calculateTotal method
         total = product1.calculateTotal()
 
         # Define the expected total
